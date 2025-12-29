c93cbbbd1554a0a6fbff3c5cd1fb1e7a6960251a
Keola Morse <keola.morse@theredcarrot.com>
2025-12-28 23:02:28 -1000

    News MP3 player MVP

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+MP3_FOLDER = os.path.join(app.root_path, "static", "mp3")
+
+@app.route("/")
+def index():
+    # List mp3 files in the folder
+    files = [
+        f for f in os.listdir(MP3_FOLDER)
+        if f.lower().endswith(".mp3")
+    ]
+    return render_template("index.html", files=files)
+
+@app.route("/mp3/<filename>")
+def serve_mp3(filename):
+    return send_from_directory(MP3_FOLDER, filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
